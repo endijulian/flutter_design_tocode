e6eec86bbd6da4aa28c11c2fd78121f0d946596b
endi julian <endi@iglobal.co.id>
2021-11-20 05:35:20 +0700

    part basic

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,8 +5,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        // SingleChildScrollView (Untuk scroll jika datanya melebihi dari luas layar)
+        child: ListView(
           children: [
             Image.asset('assets/gunung-bromoo.jpg'),
             SizedBox(
@@ -50,6 +50,83 @@ class HomePage extends StatelessWidget {
                     ],
                   )
                 ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Color(0XFF0295FF),
+                        size: 30,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'CALL',
+                        style: TextStyle(
+                          color: Color(0XFF0295FF),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.near_me,
+                        color: Color(0XFF0295FF),
+                        size: 30,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'ROUTE',
+                        style:
+                            TextStyle(color: Color(0XFF0295FF), fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.share,
+                        color: Color(0XFF0295FF),
+                        size: 30,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'SHARE',
+                        style: TextStyle(
+                          color: Color(0XFF0295FF),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                'Gunung Bromo (dari bahasa Sanskerta: Brahma, salah seorang Dewa Utama dalam agama Hindu) atau dalam bahasa Tengger dieja "Brama", adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia. Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut dan berada dalam empat wilayah kabupaten, yakni Kabupaten Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang. \n\nGunung Bromo terkenal sebagai objek wisata utama di Jawa Timur. Sebagai sebuah objek wisata, Bromo menjadi menarik karena statusnya sebagai gunung berapi yang masih aktif. Gunung Bromo termasuk dalam kawasan Taman Nasional Bromo Tengger Semeru Bentuk tubuh Gunung Bromo bertautan antara lembah dan ngarai dengan kaldera atau lautan pasir seluas sekitar 10 kilometer persegi. Ia mempunyai sebuah kawah dengan garis tengah ± 800 meter (utara-selatan) dan ± 600 meter (timur-barat). Sedangkan daerah bahayanya berupa lingkaran dengan jari-jari 4 km dari pusat kawah Bromo.Selama abad 20 dan abad 21, Gunung Bromo telah meletus sebanyak beberapa kali, dengan interval waktu yang teratur, yaitu 30 tahun. Letusan terbesar terjadi 1974, sedangkan letusan terakhir terjadi pada 2016-sekarang.Selama abad 20 dan abad 21, Gunung Bromo telah meletus sebanyak beberapa kali, dengan interval waktu yang teratur, yaitu 30 tahun. Letusan terbesar terjadi 1974, sedangkan letusan terakhir terjadi pada 2016-sekarang.',
+                style: TextStyle(fontSize: 15),
+                textAlign: TextAlign.justify,
               ),
             )
           ],
